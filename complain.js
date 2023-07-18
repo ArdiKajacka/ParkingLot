39a9c76825486bd92e650e6837992fb399b01e64
jonel 2002 <jhamzallari20@epoka.edu.al>
2023-07-18 09:28:09 +0200

    psh

---
+++ complain.js
@@ -0,0 +1,69 @@
+// Function to load complaints data using XMLHttpRequest GET request
+function loadComplaintsData() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "complaints.json", true);
+  
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var jsonResponse = JSON.parse(xhr.responseText);
+        // Process the JSON data here
+        console.log(jsonResponse);
+      }
+    };
+  
+    xhr.send();
+  }
+  
+  // Function to handle form submission using XMLHttpRequest POST request
+  function handleFormSubmission(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Get form values
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const complaintType = document.getElementById('complaintType').value;
+    const complaintDetails = document.getElementById('complaintDetails').value;
+  
+    // Create complaint object
+    const newComplaint = {
+      firstName: firstName,
+      lastName: lastName,
+      phone: phone,
+      email: email,
+      complaintType: complaintType,
+      complaintDetails: complaintDetails
+    };
+  
+    // Add the new complaint object to the 'complaints' array
+    complaintsJsonObject.complaints.push(newComplaint);
+  
+    // Convert the JavaScript object back to a JSON string
+    const updatedJsonString = JSON.stringify(complaintsJsonObject);
+  
+    // Create a new XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "YOUR_API_ENDPOINT", true); // REPLACE WITH YOUR SERVER-SIDE API ENDPOINT
+    xhr.setRequestHeader("Content-Type", "application/json");
+  
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Request successful
+        console.log(xhr.responseText);
+      }
+    };
+  
+    // Send the updated JSON string in the request body
+    xhr.send(updatedJsonString);
+  
+    // Reset the form
+    document.getElementById('complaints').reset();
+  }
+  
+  // Get the form element
+  const form = document.getElementById('complaints');
+  
+  // Add event listener for form submission
+  form.addEventListener('submit', handleFormSubmission);
+  
--- subscribe.js
+++ subscribe.js
@@ -1,113 +1,64 @@
-// JSON data
-const existingJsonString = `{
-    "subscriber": [
-      {
-        "firstName": "John",
-        "lastName": "Doe",
-        "idCard": 123456789,
-        "email": "johndoe@example.com",
-        "phone": 987654321,
-        "plateNumber": 1234,
-        "isDeleted": false
-      },
-      {
-        "firstName": "Jane",
-        "lastName": "Smith",
-        "idCard": 987654321,
-        "email": "janesmith@example.com",
-        "phone": 123456789,
-        "plateNumber": 5678,
-        "isDeleted": false
-      },
-      {
-        "firstName": "Alice",
-        "lastName": "Johnson",
-        "idCard": 111111111,
-        "email": "alicejohnson@example.com",
-        "phone": 222222222,
-        "plateNumber": 9102,
-        "isDeleted": false
-      },
-      {
-        "firstName": "Bob",
-        "lastName": "Smithson",
-        "idCard": 333333333,
-        "email": "bobsmithson@example.com",
-        "phone": 444444444,
-        "plateNumber": 2468,
-        "isDeleted": false
-      },
-      {
-        "firstName": "Emily",
-        "lastName": "Brown",
-        "idCard": 555555555,
-        "email": "emilybrown@example.com",
-        "phone": 666666666,
-        "plateNumber": 1357,
-        "isDeleted": false
-      },
-      {
-        "firstName": "Michael",
-        "lastName": "Davis",
-        "idCard": 777777777,
-        "email": "michaeldavis@example.com",
-        "phone": 888888888,
-        "plateNumber": 7890,
-        "isDeleted": false
-      },
-      {
-        "firstName": "Sophia",
-        "lastName": "Wilson",
-        "idCard": 999999999,
-        "email": "sophiawilson@example.com",
-        "phone": 101010101,
-        "plateNumber": 1111,
-        "isDeleted": false
-      }
-    ]
-  }`;
-  
-  // Parse the existing JSON data into a JavaScript object
-  const existingJsonObject = JSON.parse(existingJsonString);
-  
-  // Function to handle form submission
-  function handleFormSubmission(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    // Get form values
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const cardNumber = document.getElementById('cardNumber').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const birthday = document.getElementById('birthday').value;
-    const plateNumber = document.getElementById('plateNumber').value;
-  
-    // Create subscriber object
-    const newSubscriber = {
-      firstName: firstName,
-      lastName: lastName,
-      idCard: cardNumber,
-      email: email,
-      phone: phone,
-      birthday: birthday,
-      plateNumber: plateNumber,
-      isDeleted: false
-    };
-  
-    // Add the new subscriber object to the 'subscriber' array
-    existingJsonObject.subscriber.push(newSubscriber);
-  
-    // Convert the JavaScript object back to a JSON string
-    const updatedJsonString = JSON.stringify(existingJsonObject);
-  
-    // Log the updated JSON string (you can send it to a server or perform any other action here)
-    console.log(updatedJsonString);
-  }
-  
-  // Get the form element
-  const form = document.getElementById('myForm');
-  
-  // Add event listener for form submission
-  form.addEventListener('submit', handleFormSubmission);
-  
+function loadComplaintData() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "subscriber.json", true);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var jsonResponse = JSON.parse(xhr.responseText);
+      // Process the JSON data here
+      console.log(jsonResponse);
+
+      // Get the form element
+      const form = document.getElementById('myForm');
+
+      // Add event listener for form submission
+      form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // Get form values
+        const firstName = document.getElementById('firstName').value;
+        const lastName = document.getElementById('lastName').value;
+        const cardNumber = document.getElementById('cardNumber').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const birthday = document.getElementById('birthday').value;
+        const plateNumber = document.getElementById('plateNumber').value;
+
+        // Create subscriber object
+        const newSubscriber = {
+          firstName: firstName,
+          lastName: lastName,
+          idCard: cardNumber,
+          email: email,
+          phone: phone,
+          birthday: birthday,
+          plateNumber: plateNumber,
+          isDeleted: false
+        };
+
+        // Add the new subscriber object to the 'subscriber' array
+        jsonResponse.subscriber.push(newSubscriber);
+
+        // Convert the JavaScript object back to a JSON string
+        const updatedJsonString = JSON.stringify(jsonResponse);
+
+        // Create a new XMLHttpRequest object
+        var postRequest = new XMLHttpRequest();
+        postRequest.open("POST", "API_ENDPOINT", true); 
+        postRequest.setRequestHeader("Content-Type", "application/json");
+
+        postRequest.onreadystatechange = function () {
+          if (postRequest.readyState === 4 && postRequest.status === 200) {
+            // Request successful
+            console.log(postRequest.responseText);
+          }
+        };
+
+        // Send the updated JSON string in the request body
+        postRequest.send(updatedJsonString);
+      });
+    }
+  };
+
+  xhr.send();
+}
