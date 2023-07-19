afc42eaaed9545e3b035cc8ebbf7f7eb87aa5962
jonel 2002 <jhamzallari20@epoka.edu.al>
2023-07-19 20:01:00 +0200

    js fix

--- complain.js
+++ complain.js
@@ -1,69 +1,69 @@
-// Function to load complaints data using XMLHttpRequest GET request
-function loadComplaintsData() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "complaints.json", true);
-  
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var jsonResponse = JSON.parse(xhr.responseText);
-        // Process the JSON data here
-        console.log(jsonResponse);
-      }
-    };
-  
-    xhr.send();
-  }
-  
-  // Function to handle form submission using XMLHttpRequest POST request
-  function handleFormSubmission(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    // Get form values
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const phone = document.getElementById('phone').value;
-    const email = document.getElementById('email').value;
-    const complaintType = document.getElementById('complaintType').value;
-    const complaintDetails = document.getElementById('complaintDetails').value;
-  
-    // Create complaint object
-    const newComplaint = {
-      firstName: firstName,
-      lastName: lastName,
-      phone: phone,
-      email: email,
-      complaintType: complaintType,
-      complaintDetails: complaintDetails
-    };
-  
-    // Add the new complaint object to the 'complaints' array
-    complaintsJsonObject.complaints.push(newComplaint);
-  
-    // Convert the JavaScript object back to a JSON string
-    const updatedJsonString = JSON.stringify(complaintsJsonObject);
-  
-    // Create a new XMLHttpRequest object
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "complaints.json", true); // REPLACE WITH YOUR SERVER-SIDE API ENDPOINT
-    xhr.setRequestHeader("Content-Type", "application/json");
-  
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
+// Function to load complaints data from the JSON file
+function getComplaintsData() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "complaints.json", true);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var jsonResponse = JSON.parse(xhr.responseText);
+      // Process the JSON data here
+      console.log(jsonResponse);
+    }
+  };
+
+  xhr.send();
+}
+
+// Function to handle the form submission
+function submitComplaintData() {
+  // Get form values
+  const firstName = document.getElementById('complaintFirstName').value;
+  const lastName = document.getElementById('complaintLastName').value;
+  const phone = document.getElementById('complaintPhone').value;
+  const email = document.getElementById('complaintEmail').value;
+  const complaintType = document.getElementById('complaintType').value;
+  const complaintDetails = document.getElementById('complaintDetails').value;
+
+  // Create complaint object
+  const newComplaint = {
+    firstName: firstName,
+    lastName: lastName,
+    phone: phone,
+    email: email,
+    complaintType: complaintType,
+    complaintDetails: complaintDetails
+  };
+
+  // Convert the JavaScript object to a JSON string
+  const newComplaintJson = JSON.stringify(newComplaint);
+
+  // Create a new XMLHttpRequest object
+  var postRequest = new XMLHttpRequest();
+  postRequest.open("POST", "complaints.json", true);
+  postRequest.setRequestHeader("Content-Type", "application/json");
+
+  postRequest.onreadystatechange = function () {
+    if (postRequest.readyState === 4) {
+      if (postRequest.status === 200) {
         // Request successful
-        console.log(xhr.responseText);
+        console.log(postRequest.responseText);
+      } else {
+        // Request failed
+        console.error("Failed to submit complaint:", postRequest.responseText);
       }
-    };
-  
-    // Send the updated JSON string in the request body
-    xhr.send(updatedJsonString);
-  
-    // Reset the form
-    document.getElementById('complaints').reset();
-  }
-  
-  // Get the form element
-  const form = document.getElementById('complaints');
-  
-  // Add event listener for form submission
-  form.addEventListener('submit', handleFormSubmission);
-  
+    }
+  };
+
+  // Send the new complaint JSON string in the request body
+  postRequest.send(newComplaintJson);
+}
+
+// Load complaints data when the page loads
+getComplaintsData();
+
+// Add event listener to the form's submit button
+const submitButton = document.getElementById('complain-submit-button');
+submitButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  submitComplaintData();
+});
--- subscribe.js
+++ subscribe.js
@@ -1,4 +1,5 @@
-function loadSubscriberData() {
+// Function to load subscriber data from the JSON file
+function getSubscriberData() {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "subscriber.json", true);
 
@@ -7,60 +8,64 @@ function loadSubscriberData() {
       var jsonResponse = JSON.parse(xhr.responseText);
       // Process the JSON data here
       console.log(jsonResponse);
-
-      // Get the form element
-      const form = document.getElementById('myForm');
-
-      // Add event listener for form submission
-      form.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        // Get form values
-        const firstName = document.getElementById('firstName').value;
-        const lastName = document.getElementById('lastName').value;
-        const cardNumber = document.getElementById('cardNumber').value;
-        const email = document.getElementById('email').value;
-        const phone = document.getElementById('phone').value;
-        const birthday = document.getElementById('birthday').value;
-        const plateNumber = document.getElementById('plateNumber').value;
-
-        // Create subscriber object
-        const newSubscriber = {
-          firstName: firstName,
-          lastName: lastName,
-          idCard: parseInt(cardNumber),
-          email: email,
-          phone: phone,
-          birthday: birthday,
-          plateNumber: plateNumber,
-          isDeleted: false
-        };
-
-        // Convert the JavaScript object to a JSON string
-        const newSubscriberJson = JSON.stringify(newSubscriber);
-
-        // Create a new XMLHttpRequest object
-        var postRequest = new XMLHttpRequest();
-        postRequest.open("POST", "subscriber.json", true);
-        postRequest.setRequestHeader("Content-Type", "application/json");
-
-        postRequest.onreadystatechange = function () {
-          if (postRequest.readyState === 4) {
-            if (postRequest.status === 200) {
-              // Request successful
-              console.log(postRequest.responseText);
-            } else {
-              // Request failed
-              console.error("Failed to create subscriber:", postRequest.responseText);
-            }
-          }
-        };
-
-        // Send the new subscriber JSON string in the request body
-        postRequest.send(newSubscriberJson);
-      });
     }
   };
 
   xhr.send();
 }
+
+// Function to handle the form submission
+function submitSubscriberData() {
+  // Get form values
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+  const cardNumber = document.getElementById('cardNumber').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const birthday = document.getElementById('birthday').value;
+  const plateNumber = document.getElementById('plateNumber').value;
+
+  // Create subscriber object
+  const newSubscriber = {
+    firstName: firstName,
+    lastName: lastName,
+    idCard: parseInt(cardNumber),
+    email: email,
+    phone: phone,
+    birthday: birthday,
+    plateNumber: plateNumber,
+    isDeleted: false
+  };
+
+  // Convert the JavaScript object to a JSON string
+  const newSubscriberJson = JSON.stringify(newSubscriber);
+
+  // Create a new XMLHttpRequest object
+  var postRequest = new XMLHttpRequest();
+  postRequest.open("POST", "subscriber.json", true);
+  postRequest.setRequestHeader("Content-Type", "application/json");
+
+  postRequest.onreadystatechange = function () {
+    if (postRequest.readyState === 4) {
+      if (postRequest.status === 200) {
+        // Request successful
+        console.log(postRequest.responseText);
+      } else {
+        // Request failed
+        console.error("Failed to create subscriber:", postRequest.responseText);
+      }
+    }
+  };
+
+  // Send the new subscriber JSON string in the request body
+  postRequest.send(newSubscriberJson);
+}
+
+// Load subscriber data when the page loads
+getSubscriberData();
+
+const submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  submitSubscriberData();
+});
